18dd79aa932701f39c4349e440568fa7ff17498d
Christopher <me@cgad.ski>
2023-06-15 09:25:36 +0100

    alejandra format

--- flake.nix
+++ flake.nix
@@ -5,22 +5,26 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      with nixpkgs.lib; 
-      with nixpkgs.legacyPackages.${system};
-      {
-        packages.default = stdenv.mkDerivation rec {
-          pname = "snake-simulator";
-          version = "0.1";
-          nativeBuildInputs = [ cmake ];
-          buildInputs = [ ncurses6 boost ];
-          src = ./.;
-          installPhase = ''
-          mkdir -p $out/bin
-          cp snake $out/bin/${pname}
-          '';
-        };
-      }
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        with nixpkgs.lib;
+        with nixpkgs.legacyPackages.${system}; {
+          packages.default = stdenv.mkDerivation rec {
+            pname = "snake-simulator";
+            version = "0.1";
+            nativeBuildInputs = [cmake];
+            buildInputs = [ncurses6 boost];
+            src = ./.;
+            installPhase = ''
+              mkdir -p $out/bin
+              cp snake $out/bin/${pname}
+            '';
+          };
+        }
     );
 }
